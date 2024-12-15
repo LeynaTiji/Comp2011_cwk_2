53817cde94c39c8803eec0be314310364109cab3
Leyna Tiji <sc23lt@leeds.ac.uk>
2024-12-15 07:53:10 +0000

    changed init

--- app/__init__.py
+++ app/__init__.py
@@ -3,7 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder="templates",static_folder="static")
 app.config.from_object('config')
 app.config['DEBUG'] = True
 db = SQLAlchemy(app)
@@ -13,8 +13,8 @@ login_manager = LoginManager()
 login_manager.init_app(app)
 login_manager.login_view = 'login'
 
-from app import views, models
-from app.models import User, Film
+from app import views, models, forms
+from app.models import Film
 
 with app.app_context():
     if Film.query.count() == 0:
